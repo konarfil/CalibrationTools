9ccbbe32bbdbcac338c210fb6c94cd6924229967
konarfil <125542326+konarfil@users.noreply.github.com>
2023-03-09 11:51:47 +0100

    Update temp_sub.sh

--- build/temp_sub.sh
+++ build/temp_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --mem      8192M
 #SBATCH --licenses sps
-#SBATCH --time     5:00:00
+#SBATCH --time     12:00:00
 #SBATCH --ntasks   1
 
 ./exctract_OM_spectra -i $RED_PATH/snemo_run-%RUN_NUMBER_red-v2.data.gz -o %OUTPUT_FOLDER -r %RUN_NUMBER -n %N_EVENTS
