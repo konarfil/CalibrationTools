bedccc10c3199dbc53c71f46948b1da33d2307ce
konarfil <fik.konarik@gmail.com>
2024-07-04 15:09:52 +0200

    Delete old files

--- build/temp_sub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#SBATCH --mem      8192M
-#SBATCH --licenses sps
-#SBATCH --time     12:00:00
-#SBATCH --ntasks   1
-
-./exctract_OM_spectra -i $RED_PATH/snemo_run-%RUN_NUMBER_red-v2.data.gz -o %OUTPUT_FOLDER -r %RUN_NUMBER -n %N_EVENTS
-root '../fit_spectra.cpp("%OUTPUT_FOLDER", "%FIT_FOLDER")'
